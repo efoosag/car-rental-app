97e75cf6357d9edee009c99d79fa689ac516f0d1
bruk19 <brukteshome21@gmail.com>
2022-12-06 15:52:25 +0300

    Add car components and distructuring cars

--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-function Car() {
+function Car(car) {
+
+    const {
+        name, model, feature, photo,
+      } = car;
   
     return (
       <>
         <div className="car-box">
-          <h3 className="car-name"></h3>
-          <h4 className="car-title"></h4>
+          <img className="car-image" src={photo} alt={name} />
+          <h3 className="car-name">{name}</h3>
+          <h4 className="car-title">{model}</h4>
         </div>
       </>
   
