a004501ee1243cf4784bffa0f6783b012d6970e4
Gideon <gideons564@gmail.com>
2022-12-15 14:24:47 +0100

    correct linter errors

--- src/components/Car/Cars.js
+++ src/components/Car/Cars.js
@@ -1,10 +1,10 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from 'react-router-dom';
-import { getCar } from '../../store/car';
-import { getCars } from '../../store/detail';
+import { Link } from "react-router-dom";
+import { getCar } from "../../store/car";
+import { getCars } from "../../store/detail";
 import "./Cars.css";
 
 const Cars = () => {
@@ -17,13 +17,13 @@ const Cars = () => {
     dispatch(getCar());
   }, [dispatch]);
 
-  const carsTest = (arr) => arr.slice(initial, initial + 3);
+  const carsTest = (arr) => arr.slice(initial, initial + 20);
   const nextCars = carsTest(cars || []);
   const chooseName = () => {
     if (right === true) {
-      return 'first';
+      return "first";
     }
-    return 'second';
+    return "second";
   };
   const classNaming = chooseName();
   return (
@@ -60,9 +60,7 @@ const Cars = () => {
                 <h3 className="model-name">{car.name}</h3>
                 <h3 className="car-info">{car.model}</h3>
                 <h3 className="car-info">{car.feature}</h3>
-
               </Link>
-
             </div>
           ))}
         </ul>
